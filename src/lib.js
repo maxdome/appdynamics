b8dfe3a5dea62ead35be70235e90ae7c272be343
Christian Neuhäuser <chris@type10.com>
2017-09-04 12:03:42 +0200

    Implements try/catch and basic logging

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = (config) => {
+module.exports = (config) => { // TODO: It would be nice to inject the logger here
   if (process.env.MXD_APPDYNAMICS) {
     config.appdynamics = {
       active: process.env.MXD_APPDYNAMICS_ACTIVE,
@@ -8,9 +8,12 @@ module.exports = (config) => {
     };
   }
 
-  if (config.appdynamics &&
-      config.appdynamics.active &&
-      config.appdynamics.profile) {
-    require('appdynamics').profile(config.appdynamics.profile);
+  if (config.appdynamics && config.appdynamics.active && config.appdynamics.profile) {
+    try {
+      console.log('Enable appdynamics', config.appdynamics.profile); // TODO: It would be nicer to log through an injected logger
+      require('appdynamics').profile(config.appdynamics.profile);
+    } catch (err) {
+      console.log('appdynamics config error', err); // TODO: It would be nicer to log through an injected logger
+    }
   }
 };
